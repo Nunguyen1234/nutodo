26c01d205973bd51e789900aa895db9bd0ba5628
nunguyen31 <nguyenu3107@gmail.com>
2025-06-11 23:13:27 +0700

    fix

--- components/Helper/SearchBox.tsx
+++ components/Helper/SearchBox.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaCalendarWeek, FaMap } from "react-icons/fa";
 import { FaUserGroup } from "react-icons/fa6";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const SearchBox = () => {
   return (
    <div className="bg-white rounded-lg p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center justify-center gap-8 mt-4 sm:mt-12 w-[95%] sm:w-[80%] transition-all transform hover:scale-105 hover:-translate-y-1 ease-out duration-500 hover:ring-2 hover:ring-offset-2 hover:bg-gradient-to-r hover:from-red-300 hover:to-yellow-300 overflow-hidden group">
   {/* 1st search input */}
   <div className="flex items-center space-x-6">
-    <FaMap className="w-6 h-6 text-blue-600" />
+    <FaMapMarkerAlt  className="w-6 h-6 text-blue-600" />
     <div>
       <p className="text-lg font-medium mb-[0.2rem] text-black group-hover:text-white">Location</p>
       <input
